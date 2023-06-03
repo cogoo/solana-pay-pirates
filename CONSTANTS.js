a94ccd9ca911c3cb9984459f1c83b184d99cab86
Colin Ogoo <ogoo.colin@gmail.com>
2023-06-03 19:42:51 -0400

    chore: renaming to lowercase

--- CONSTANTS.js
+++
@@ -1,4 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-
-export const SEVEN_SEAS_PROGRAM = new PublicKey('2a4NcnkF5zf14JQXHAv39AsRf7jMFj13wKmTL6ZcDQNd');
-export const GOLD_TOKEN_MINT = new PublicKey('goLdQwNaZToyavwkbuPJzTt5XPNR3H7WQBGenWtzPH3');
