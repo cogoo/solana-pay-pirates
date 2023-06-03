7e8e88f020dd6b436802f1d347bdf21ac1db5390
Colin Ogoo <ogoo.colin@gmail.com>
2023-06-03 16:32:32 -0400

    refactor: extract constants and inner functions

--- api/index.js
+++ api/index.js
@@ -11,27 +11,37 @@ import {
   SystemProgram,
   Transaction,
   TransactionInstruction,
-  sendAndConfirmTransaction,
 } from '@solana/web3.js';
 import bs58 from 'bs58';
 import dotenv from 'dotenv';
+import { SEVEN_SEAS_PROGRAM, GOLD_TOKEN_MINT } from '../CONSTANTS';
 
+// Load environment variables from .env
 dotenv.config();
 
-const SEVEN_SEAS_PROGRAM = new PublicKey('2a4NcnkF5zf14JQXHAv39AsRf7jMFj13wKmTL6ZcDQNd');
-const GOLD_TOKEN_MINT = new PublicKey('goLdQwNaZToyavwkbuPJzTt5XPNR3H7WQBGenWtzPH3');
 const connection = new Connection('https://api.devnet.solana.com', 'confirmed');
-// @ts-ignore
 const payer = Keypair.fromSecretKey(bs58.decode(process.env.PAYER));
+
+/**
+ * @param {VercelResponse} response
+ */
+function handleGet(response) {
+  return response.status(200).json({
+    label: 'Chutulu Fire!',
+    icon: 'https://github.com/solana-developers/pirate-bootcamp/blob/main/assets/kraken-1.png?raw=true',
+  });
+}
 
 /**
  * @param {VercelRequest} request
  * @param {VercelResponse} response
  */
 async function handlePost(request, response) {
+  // get wallet address
   const player = new PublicKey(request.body.account);
   console.log('player', player.toBase58());
 
+  // start: get program derived addresses
   const [level] = PublicKey.findProgramAddressSync([Buffer.from('level')], SEVEN_SEAS_PROGRAM);
 
   const [chestVault] = PublicKey.findProgramAddressSync(
@@ -53,6 +63,7 @@ async function handlePost(request, response) {
     [Buffer.from('token_vault', 'utf8'), GOLD_TOKEN_MINT.toBuffer()],
     SEVEN_SEAS_PROGRAM,
   );
+  // end: get program derived addresses
 
   const playerTokenAccount = await getOrCreateAssociatedTokenAccount(
     connection,
@@ -61,7 +72,86 @@ async function handlePost(request, response) {
     player,
   );
 
-  const chutuluIX = new TransactionInstruction({
+  // create chutulu instruction
+  const chutuluIX = createChutuluIx(
+    chestVault,
+    level,
+    gameActions,
+    player,
+    playerTokenAccount,
+    tokenVault,
+    tokenAccountOwnerPda,
+  );
+
+  let tx = new Transaction().add(chutuluIX);
+  tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+  tx.feePayer = payer.publicKey;
+
+  // partial sign transaction
+  tx.partialSign(payer);
+
+  tx = Transaction.from(
+    tx.serialize({
+      verifySignatures: false,
+      requireAllSignatures: false,
+    }),
+  );
+
+  const serializedTx = tx.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
+  });
+
+  const base64 = serializedTx.toString('base64');
+
+  return response.status(200).json({
+    transaction: base64,
+    message: 'Chutulu Fire!',
+  });
+}
+
+/**
+ * @typedef {import('@vercel/node').VercelResponse} VercelResponse
+ * @typedef {import('@vercel/node').VercelRequest} VercelRequest
+ *
+ * @param {VercelRequest} request
+ * @param {VercelResponse} response
+ * @returns {Promise<VercelResponse>}
+ * */
+export default async function handler(request, response) {
+  console.log('handling request', request.method);
+
+  if (request.method === 'GET') {
+    return handleGet(response);
+  } else if (request.method === 'POST') {
+    return await handlePost(request, response);
+  } else {
+    return response.status(405).json({ error: 'Method not allowed' });
+  }
+}
+
+/**
+ * @typedef {import('@solana/spl-token').Account} Account
+ *
+ * @param {PublicKey} chestVault
+ * @param {PublicKey} level
+ * @param {PublicKey} gameActions
+ * @param {PublicKey} player
+ * @param {Account} playerTokenAccount
+ * @param {PublicKey} tokenVault
+ * @param {PublicKey} tokenAccountOwnerPda
+ * @returns {TransactionInstruction}
+ */
+function createChutuluIx(
+  chestVault,
+  level,
+  gameActions,
+  player,
+  playerTokenAccount,
+  tokenVault,
+  tokenAccountOwnerPda,
+) {
+  return new TransactionInstruction({
     programId: SEVEN_SEAS_PROGRAM,
     keys: [
       {
@@ -125,61 +215,7 @@ async function handlePost(request, response) {
         isSigner: false,
       },
     ],
-
+    // discriminator for the chutulu instruction
     data: Buffer.from(new Uint8Array([84, 206, 8, 255, 98, 163, 218, 19, 1])),
   });
-
-  let tx = new Transaction().add(chutuluIX);
-  tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-  tx.feePayer = payer.publicKey;
-  tx.partialSign(payer);
-
-  tx = Transaction.from(
-    tx.serialize({
-      verifySignatures: false,
-      requireAllSignatures: false,
-    }),
-  );
-
-  const serializedTx = tx.serialize({
-    verifySignatures: false,
-    requireAllSignatures: false,
-  });
-
-  const base64 = serializedTx.toString('base64');
-
-  return response.status(200).json({
-    transaction: base64,
-    message: 'Chutulu Fire!',
-  });
-}
-
-/**
- * @param {VercelResponse} response
- */
-function handleGet(response) {
-  return response.status(200).json({
-    label: 'Chutulu Fire!',
-    icon: 'https://github.com/solana-developers/pirate-bootcamp/blob/main/assets/kraken-1.png?raw=true',
-  });
-}
-
-/**
- * @typedef {import('@vercel/node').VercelResponse} VercelResponse
- * @typedef {import('@vercel/node').VercelRequest} VercelRequest
- *
- * @param {VercelRequest} request
- * @param {VercelResponse} response
- * @returns {Promise<VercelResponse>}
- * */
-export default async function handler(request, response) {
-  console.log('handling request', request.method);
-
-  if (request.method === 'GET') {
-    return handleGet(response);
-  } else if (request.method === 'POST') {
-    return await handlePost(request, response);
-  } else {
-    return response.status(405).json({ error: 'Method not allowed' });
-  }
 }
